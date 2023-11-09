33f20b1cfa13970ac4ce5c2c9afd12830b6b2020
azkarmoulana <Azkar.Moulana@rabobank.nl>
2023-11-09 14:58:05 +0100

    setup project architecture and configurations

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/src/test.ts',
+  ],
+  moduleNameMapper: {
+    '@Components': '<rootDir>/src/app/shared/components',
+    '@Animations': '<rootDir>/src/app/shared/animations',
+    '@Utils': '<rootDir>/src/app/shared/utils',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
+};
