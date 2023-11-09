a83756420fbbe7a2026e40042204e437ff4a46be
azkarmoulana <Azkar.Moulana@rabobank.nl>
2023-11-09 16:12:18 +0100

    get new user every 5 s

--- src/app/modules/users/components/users/users.component.ts
+++ src/app/modules/users/components/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { getRandomUserId } from '@Utils';
 import { IUser } from '../../models/user';
 import { DataService } from '../../services/data.service';
 import { UsersService } from '../../services/users.service';
@@ -8,9 +10,11 @@ import { UsersService } from '../../services/users.service';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
   initialUserCount = 10;
   users: IUser[] = [];
+  apiReq$ = interval(5000);
+  apiReqSubscription: Subscription = new Subscription();
 
   constructor(
     private userService: UsersService,
@@ -29,7 +33,23 @@ export class UsersComponent implements OnInit {
           });
       }
     });
+
+    this.requestNewUser();
+  }
+
+  requestNewUser(): void {
+    this.apiReqSubscription = this.apiReq$.subscribe(e => {
+      // Generating a random userId between 1 - 12
+      const newUserId = getRandomUserId(1, 12);
+      this.dataService.getNewUser(newUserId).subscribe((data: any) => {
+        this.userService.setUser(data);
+      });
+    });
   }
 
   onFavUserSelect($event: any) {}
+
+  ngOnDestroy() {
+    this.apiReqSubscription.unsubscribe();
+  }
 }
